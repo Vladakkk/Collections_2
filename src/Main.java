cbb5d1d4ae9fa0578b67f8684c7ef89335dca057
Vladakkk <vladakasperskaa@gmail.com>
2024-03-27 19:53:17 +0200

    Collections homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import phonebook.Entry;
+import phonebook.PhoneBook;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.add(new Entry("Оксана Бойко", "0674567832"));
+        phoneBook.add(new Entry("Петро Колісник", "0986754521"));
+        phoneBook.add(new Entry("Оксана Бойко", "0674573489"));
+        phoneBook.add(new Entry("Олег Швачко", "0662117633"));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть ім'я для пошуку: ");
+        String findName = scanner.nextLine();
+        Entry foundEntry = phoneBook.find(findName);
+        if (foundEntry != null) {
+            System.out.println("Знайдено запис для " + findName + ": " + foundEntry);
+        } else {
+            System.out.println("Запис для " + findName + " не знайдено.");
+        }
     }
 }
